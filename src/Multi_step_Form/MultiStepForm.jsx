1284f2cf0f4e00b849d466896858f370d748bb0e
mahaprabhu <mahaprabhu1994@gmail.com>
2023-05-19 11:34:17 +0530

    multi step-form-stage initial

--- src/Multi_step_Form/MultiStepForm.jsx
+++ src/Multi_step_Form/MultiStepForm.jsx
@@ -3,16 +3,60 @@ import React, { useState } from 'react'
 function MultiStepForm() {
     const [step, setStep] = useState(0);
 
+
+    const renderBtn = () => {
+        if (step > 2) {
+            return (
+                <>
+                    nice !!
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <button type='button' onClick={() => { setStep(step + 1) }} >Next Step</button>
+                </>
+            )
+        }
+    }
+
     console.log(step)
     return (
         <div>
             <h1>Multi-step</h1>
-            {
-                step
-            }
             <form>
-                <input type='text' className='bg-green-100' />
-                <input type='submit' onClick={() => { setStep(step + 1) }} />
+                {
+                    step === 0 &&
+                    (
+                        <section>
+                            <input type='text' className='bg-green-100' placeholder='name' />
+                        </section>
+                    )
+                }
+                {
+                    step === 1 && (
+                        <section>
+                            <input type='text' className='bg-green-100' placeholder='dept' />
+                        </section>
+                    )
+                }
+                {
+                    step === 2 && (
+                        <section>
+                            <input type='checkbox' />
+                            <label>Please agree the tearms and conditions</label>
+                        </section>
+                    )
+                }
+                {
+                    step === 3 && (
+                        <section>
+                            <h1>Success</h1>
+                        </section>
+                    )
+                }
+                {renderBtn()}
+
             </form>
         </div>
     )
