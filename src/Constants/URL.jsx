c34e9de3f1285adfe5a5d45f801573fc105f5a12
mahaprabhu <mahaprabhu1994@gmail.com>
2023-05-19 22:44:23 +0530

    multistep_basic-setup finish

--- src/Constants/URL.jsx
+++ src/Constants/URL.jsx
@@ -1,2 +1,3 @@
 export const API_USER = "http://localhost:8000/user/";
 export const API_NESTED_OBJ = "http://localhost:8000/nestedObj/";
+export const API_MULTI = "http://localhost:8000/multi/";
--- src/Multi_step_Form/MultiStepForm.jsx
+++ src/Multi_step_Form/MultiStepForm.jsx
@@ -1,53 +1,85 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { API_MULTI } from '../Constants/URL';
+
 
 function MultiStepForm() {
     const [step, setStep] = useState(0);
+    const [Values, setValues] = useState();
+    const { register, handleSubmit, watch, formState: { errors, isValid } } = useForm({ mode: "all" });
+
 
 
     const renderBtn = () => {
-        if (step > 2) {
+        if (step === 2) {
             return (
                 <>
-                    nice !!
+                    <button type='submit' onClick={() => { setStep(step + 1) }} >Create</button>
+                </>
+            )
+        }
+        else if (step > 2) {
+            return (
+                <>
+                    <button type='button' onClick={() => { window.location.reload(true) }}>
+                        <h1>Go Back to Home</h1>
+                    </button>
                 </>
             )
         } else {
             return (
                 <>
-                    <button type='button' onClick={() => { setStep(step + 1) }} >Next Step</button>
+                    <button
+                        disabled={!isValid}
+                        type='button' onClick={() => { setStep(step + 1) }} >Next Step</button>
                 </>
             )
         }
     }
 
-    console.log(step)
+    const onSubmit = (value) => {
+        axios.post(API_MULTI, value)
+        setValues(value)
+    }
+    console.log("form data", Values)
     return (
         <div>
             <h1>Multi-step</h1>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 {
                     step === 0 &&
                     (
                         <section>
-                            <input type='text' className='bg-green-100' placeholder='name' />
+                            <input
+
+                                {...register("name", { required: true })}
+                                aria-invalid={errors.name ? "true" : "false"}
+                                type='text' className='bg-green-100' placeholder='name' />
+                            {errors.name?.type === 'required' && <p role="alert" className='text-red-400'>Name is required</p>}
                         </section>
                     )
                 }
                 {
                     step === 1 && (
                         <section>
-                            <input type='text' className='bg-green-100' placeholder='dept' />
+                            <input
+                                {...register("dept")}
+                                type='text' className='bg-green-100' placeholder='dept' />
                         </section>
                     )
                 }
                 {
                     step === 2 && (
                         <section>
-                            <input type='checkbox' />
+                            <input
+                                {...register("agree")}
+                                type='checkbox' />
                             <label>Please agree the tearms and conditions</label>
                         </section>
                     )
                 }
+                {renderBtn()}
                 {
                     step === 3 && (
                         <section>
@@ -55,9 +87,12 @@ function MultiStepForm() {
                         </section>
                     )
                 }
-                {renderBtn()}
 
             </form>
+
+            <pre>
+                {JSON.stringify(watch(), null, 2)}
+            </pre>
         </div>
     )
 }
